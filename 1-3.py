91884a307536fc39f8e182bf4bd232b6de5d78ca
王栩隐 <wang@ai.cs.ehime-u.ac.jp>
2025-11-02 19:23:12 +0900

    Update 1-3.py

--- 1-3.py
+++ 1-3.py
@@ -1,6 +1,3 @@
-import re
-
-sentence = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-words = re.findall(r'\b\w+\b', sentence.lower())
-word_lengths = [len(word) for word in words]
-print(word_lengths)
+s = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+words = s.split()
+print(words)
