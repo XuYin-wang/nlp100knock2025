03831290ce3328adf8bf627db71907a506b7c059
王栩隐 <wang@ai.cs.ehime-u.ac.jp>
2025-11-12 14:34:22 +0900

    Update 2-0.py

--- chapter2/2-0.py
+++ chapter2/2-0.py
@@ -5,4 +5,4 @@ with open('popular-names.txt', 'r', encoding='utf-8') as file:
     line_count = sum(1 for line in file)
 print(f"ファイルの行数: {line_count}")
 
-#実行確認$ wc -l 2.UNIXコマンド/popular-names.txt
+#実行確認$ wc -l popular-names.txt
