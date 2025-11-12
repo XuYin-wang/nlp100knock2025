e3eaf8be6fbda06f3f0a7ae2c01c1298cac5c5e0
王栩隐 <wang@ai.cs.ehime-u.ac.jp>
2025-11-12 10:40:32 +0900

    Update 2-0.py

--- chapter2/2-0.py
+++ chapter2/2-0.py
@@ -1,1 +1,8 @@
 
+#10. 行数のカウント
+#ファイルの行数をカウントせよ。確認にはwcコマンドを用いよ。
+with open('popular-names.txt', 'r', encoding='utf-8') as file:
+    line_count = sum(1 for line in file)
+print(f"ファイルの行数: {line_count}")
+
+#実行確認$ wc -l 2.UNIXコマンド/popular-names.txt
